26d9a3c06bc53b21ce1e69194f9df37e27cd04e4
CondeGago <119528050+CondeGago@users.noreply.github.com>
2023-09-01 17:47:19 +0200

    First implementation of %c convertion

--- main.c
+++ main.c
@@ -1,1 +1,32 @@
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdarg.h>
+#include "Users/cnunez-v/limpios/proj1/libft.h"
 
+int ft_printf(char const *conv, ...)
+{
+	int	c;
+	va_list	args;
+	va_start (args, conv);
+
+	while (*conv)
+	{
+		if(*conv == '%')
+			conv++;
+		if(*conv == 'c')
+		{
+			c = va_arg(args, int);
+			putchar(c);
+		}
+		else
+			putchar(*conv);
+		conv++;
+	}
+	va_end(args);
+}
+
+int main()
+{
+	char letra = 'A';
+	ft_printf("Letra: %c", letra);
+}
